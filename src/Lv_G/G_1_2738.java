e8e1f82eff17259fc5fe16f8dfaab27c8bd027df
Young-woo <rladuddn9955@naver.com>
2026-01-09 19:13:27 +0900

    algo

---
+++ src/Lv_G/G_1_2738.java
@@ -0,0 +1,39 @@
+package Lv_G;
+
+import java.util.Scanner;
+
+public class G_1_2738 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int [][] arr1 = new int[A][B];
+        int [][] arr2 = new int[A][B];
+        int [][] result = new int[A][B];
+
+        for (int i =0; i<A; i++){
+            for (int j = 0; j<B; j++){
+                arr1[i][j] = sc.nextInt();
+            }
+        }
+        for (int i =0; i<A; i++){
+            for (int j = 0; j<B; j++){
+                arr2[i][j] = sc.nextInt();
+            }
+        }
+        for (int i =0; i<A; i++){
+            for (int j = 0; j<B; j++){
+                result[i][j] = arr1[i][j]+arr2[i][j];
+            }
+        }
+
+        for (int i =0; i<A; i++){
+            for (int j = 0; j<B; j++){
+                System.out.print(result[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/Lv_G/G_3_10798.java
@@ -0,0 +1,34 @@
+package Lv_G;
+
+import java.util.Scanner;
+
+public class G_3_10798 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        char [][] arr = new char[5][15];
+        int max = 0;
+
+        for(int i = 0; i<5; i++){
+            String str = sc.next();
+
+            if (max < str.length()){
+                max = str.length();
+            }
+            for(int j = 0;j<str.length();j++){
+                arr[i][j] = str.charAt(j);
+            }
+        }
+
+        for(int i = 0; i<max; i++){
+            for(int j = 0; j<5;j++){
+                if(arr[j][i]=='\0'){
+                    continue;
+                }
+                System.out.print(arr[j][i]);
+            }
+        }
+
+    }
+}
